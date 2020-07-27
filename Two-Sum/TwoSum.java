4319b5fdfd7d5af650b15a295db9ba571ca45799
SelesCalin <calinseles30@gmail.com>
2020-07-27 16:01:59 +0300

    two-sum

---
+++ Two-Sum/TwoSum.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.HashMap;
+/*
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> check = new HashMap<>();
+        for (int i=0; i<nums.length ; i++){
+            int pairNumber=target-nums[i];
+            if (check.containsKey(pairNumber)) {
+                return new int[]{check.get(pairNumber),i};
+            }
+            else {
+                check.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
